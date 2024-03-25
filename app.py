f285507eebcc54acb6c73013ea4a6ce4b956a393
Usman Siddiqui <usmansiddiqui51@gmail.com>
2024-03-25 14:39:59 +0500

    solved issues

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 eu_north_1 = 'eu-north-1'
 
 # 1Password credentials and vault details
-op_connect_token = os.getenv("ONEPASSWORD_API_TOKEN")
+op_connect_token = os.getenv("1PASSWORD_API_TOKEN")
 default_vault = "POC"
 connect_host = "https://my.1password.com/home"
 
@@ -38,7 +38,7 @@ def fetch_parameters_by_prefix(prefix):
 
 # Upload parameters to 1Password
 def upload_parameters_to_1password(parameters):
-   
+    print (op_connect_token)
     for key, value in parameters.items():
         user_item = Item(
             title= key,
