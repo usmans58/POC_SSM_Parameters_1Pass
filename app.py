47b779d81cdad04ef4c782665b26cf4217060ec1
Usman Siddiqui <usmansiddiqui51@gmail.com>
2024-03-27 16:21:55 +0500

    BackingUP Parameters on Repo

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
-import time
+import json
 import boto3
-import onepasswordconnectsdk
-from onepasswordconnectsdk.models import Field, GeneratorRecipe, Item,ItemVault
+from github import Github
+
+
 
 
 
@@ -10,15 +11,6 @@ from onepasswordconnectsdk.models import Field, GeneratorRecipe, Item,ItemVault
 aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
 aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 eu_north_1 = 'eu-north-1'
-
-# 1Password credentials and vault details
-op_connect_token = os.getenv("1PASSWORD_API_TOKEN")
-default_vault = "POC"
-connect_host = "https://my.1password.com/home"
-
-client = onepasswordconnectsdk.client.new_client_from_environment(connect_host)
-
-
 
 # Initialize AWS SSM client
 ssm_client = boto3.client('ssm', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=eu_north_1)
@@ -36,24 +28,18 @@ def fetch_parameters_by_prefix(prefix):
         print(f'Parameter Name: {param["Name"]}, Value: {param["Value"]}')
     return parameters
 
-# Upload parameters to 1Password
-def upload_parameters_to_1password(parameters):
-    print (op_connect_token)
-    for key, value in parameters.items():
-        user_item = Item(
-            title= key,
-            category= 'LOGIN',
-            fields= [
-                Field(
-                    value= value,
-                    purpose= 'PASSWORD'
-                
-                )  
-            ]
-        )
-    posted_item = client.create_item(default_vault, user_item)
-    time.sleep(5)
+def upload_parameters_to_repo(parameters):
+    # Write parameters to JSON file
+    with open('parameters.json', 'w') as json_file:
+        json.dump(parameters, json_file, indent=4)
 
+    # Commit JSON file to repository
+    github_token = os.getenv('GITHUB_TOKEN')
+    repo_name = os.getenv('GITHUB_REPOSITORY')
+    g = Github(github_token)
+    repo = g.get_repo(repo_name)
+    repo.create_file('parameters.json', 'Upload parameters to repository', json.dumps(parameters), branch='main')
+    print('Parameters uploaded to repository successfully.')
 
        
 
@@ -61,4 +47,5 @@ if __name__ == "__main__":
     prefix = 'V8'
     parameters = fetch_parameters_by_prefix(prefix)
     print(parameters.items())
-    upload_parameters_to_1password(parameters)
+    upload_parameters_to_repo(parameters)
+
